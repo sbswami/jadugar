6cb8de44a2000aae3da30cc854fa3c6e98c22481
Prashant-123 <pk021998@gmail.com>
2024-09-20 03:41:42 +0530

    logo fetching from mys added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json());
 
 app.use("/", require("./routes/aws"));
-app.use("/sign", require("./routes/sign"));
+app.use("/", require("./routes/assets"));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- routes/assets.js
+++ routes/assets.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
+const { default: axios } = require("axios");
 const { createCanvas, registerFont } = require("canvas");
 const fs = require("fs");
 const sharp = require("sharp");
@@ -7,7 +8,7 @@ const sharp = require("sharp");
 const fontPath = "GreatVibes-Regular.ttf";
 registerFont(fontPath, { family: "GreatVibes" });
 
-router.post("/", (req, res) => {
+router.post("/sign", (req, res) => {
   const canvas = createCanvas(300, 110);
   const ctx = canvas.getContext("2d");
 
@@ -36,6 +37,37 @@ router.post("/", (req, res) => {
   fs.writeFileSync("sign.png", buffer);
 
   res.json({ message: "Signature created successfully." });
+});
+
+router.post("/logo", async (req, res) => {
+  const { prompt } = req.body;
+
+  const mysResponse = await axios.post(
+    "https://api.niocard.in/api/generateImage",
+    {
+      prompt: prompt
+    },
+    {
+      headers: {
+        client: "android",
+        "device-id": "UE1A.230829.036.A4",
+        "client-version": "27",
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2NWM1Nzk1OTMxN2MyZDc4OTQwZjQyMDYiLCJleHAiOjE3NTgyODAzMTMsInVzZXJJZCI6IjY1YzU3OTU5MzE3YzJkNzg5NDBmNDIwNiJ9.M9tbCMIDGcaxxAWIbHs8QIqMdt9E-yr2TnEziYlWRXc"
+      }
+    }
+  );
+
+  try {
+    const response = await axios.get(mysResponse.data.url, { responseType: "arraybuffer" });
+    const contentType = response.headers["content-type"];
+    res.set("Content-Type", contentType);
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching image");
+  }
 });
 
 module.exports = router;
