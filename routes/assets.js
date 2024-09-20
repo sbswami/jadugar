7c00b6c554daba357b254bf94f716fb9e50e0f1e
Shyam Bihari Swami <sbswami24@gmail.com>
2024-09-20 06:14:58 +0530

    Update logo API

--- routes/assets.js
+++ routes/assets.js
@@ -1,43 +1,43 @@
 const router = require("express").Router();
 
 const { default: axios } = require("axios");
-const { createCanvas, registerFont } = require("canvas");
+// const { createCanvas, registerFont } = require("canvas");
 const fs = require("fs");
 const sharp = require("sharp");
 
 const fontPath = "GreatVibes-Regular.ttf";
-registerFont(fontPath, { family: "GreatVibes" });
+// registerFont(fontPath, { family: "GreatVibes" });
 
-router.post("/sign", (req, res) => {
-  const canvas = createCanvas(300, 110);
-  const ctx = canvas.getContext("2d");
+// router.post("/sign", (req, res) => {
+//   const canvas = createCanvas(300, 110);
+//   const ctx = canvas.getContext("2d");
 
-  // Set the canvas background to transparent (default, so no need to set explicitly)
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   // Set the canvas background to transparent (default, so no need to set explicitly)
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Set the font style for the signature
-  ctx.font = '56px "GreatVibes"'; // Use the custom cursive font
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
+//   // Set the font style for the signature
+//   ctx.font = '56px "GreatVibes"'; // Use the custom cursive font
+//   ctx.textAlign = "center";
+//   ctx.textBaseline = "middle";
 
-  // Add a text shadow for a more realistic effect
-  ctx.shadowColor = "rgba(0, 0, 0, 0.3)";
-  ctx.shadowOffsetX = 3;
-  ctx.shadowOffsetY = 3;
-  ctx.shadowBlur = 5;
+//   // Add a text shadow for a more realistic effect
+//   ctx.shadowColor = "rgba(0, 0, 0, 0.3)";
+//   ctx.shadowOffsetX = 3;
+//   ctx.shadowOffsetY = 3;
+//   ctx.shadowBlur = 5;
 
-  // Set text color to black
-  ctx.fillStyle = "#000000";
+//   // Set text color to black
+//   ctx.fillStyle = "#000000";
 
-  // Draw the text (name) on the canvas
-  ctx.fillText(req.body.name, canvas.width / 2, canvas.height / 2); // Replace 'Your Name Here' with actual text
+//   // Draw the text (name) on the canvas
+//   ctx.fillText(req.body.name, canvas.width / 2, canvas.height / 2); // Replace 'Your Name Here' with actual text
 
-  // Save the canvas image as a PNG file with a transparent background
-  const buffer = canvas.toBuffer("image/png");
-  fs.writeFileSync("sign.png", buffer);
+//   // Save the canvas image as a PNG file with a transparent background
+//   const buffer = canvas.toBuffer("image/png");
+//   fs.writeFileSync("sign.png", buffer);
 
-  res.json({ message: "Signature created successfully." });
-});
+//   res.json({ message: "Signature created successfully." });
+// });
 
 router.post("/logo", async (req, res) => {
   const { prompt } = req.body;
@@ -60,14 +60,19 @@ router.post("/logo", async (req, res) => {
   );
 
   try {
-    const response = await axios.get(mysResponse.data.url, { responseType: "arraybuffer" });
-    const contentType = response.headers["content-type"];
-    res.set("Content-Type", contentType);
-    res.send(response.data);
+    res.status(200).json({ url: mysResponse.data.url });
   } catch (error) {
     console.error(error);
     res.status(500).send("Error fetching image");
   }
+});
+
+router.post("/logo-file", async (req, res) => {
+  const { url } = req.body;
+  const response = await axios.get(url, { responseType: "arraybuffer" });
+  const contentType = response.headers["content-type"];
+  res.set("Content-Type", contentType);
+  res.send(response.data);
 });
 
 module.exports = router;
